cfafed5fd05990138cf505d814ccdcce7ca6a9e3
Jonathan Wrede <wrede.jonathan00@gmail.com>
2021-02-28 13:13:00 +0100

    Added Sender:

---
+++ Sender/main.py
@@ -0,0 +1,20 @@
+# import LoRaSender
+import LoRaSender
+from controller import Controller
+import sx127x
+import gc
+
+
+def main():
+    controller = Controller()
+
+    lora = controller.add_transceiver(sx127x.SX127x(name='LoRa'),
+                                      pin_id_ss=Controller.PIN_ID_FOR_LORA_SS,
+                                      pin_id_RxDone=Controller.PIN_ID_FOR_LORA_DIO0)
+
+    LoRaSender.send(lora)
+
+
+if __name__ == '__main__':
+    gc.collect()
+    main()
